6458a57c9f25f62fd6abcf580df6125d9a7ff7e4
Andrew Breckenridge <asbreckenridge@me.com>
2015-01-14 19:29:25 -0800

    switch to jade, make js and structure views

--- app.js
+++ app.js
@@ -1,15 +1,30 @@
 var express = require('express')
-var app = express()
+, routes = require('./routes')
+, http = require('http')
+, path = require('path')
+, bodyParser = require('body-parser')
+, favicon = require('serve-favicon')
+, logger = require('morgan')
+, methodOverride = require('method-override');
 
-app.get('/', function (req, res) {
-  res.send('Hello World!')
-})
+var app = express();
 
-var server = app.listen(5000, function () {
+app.set('port', process.env.PORT || 3000);
+app.set('views', __dirname + '/views');
+app.set('view engine', 'jade');
+app.use(favicon(__dirname + '/public/images/favicon.png'));
+app.use(logger('dev'));
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(methodOverride('_method'));
+app.use(require('stylus').middleware(__dirname + '/public'));
+app.use(express.static(path.join(__dirname, 'public')));
 
-  var host = server.address().address
-  var port = server.address().port
+if (app.get('env') == 'development') {
+  app.locals.pretty = true;
+}
 
-  console.log('Example app listening at http://%s:%s', host, port)
+app.get('/', routes.index);
 
-})
+http.createServer(app).listen(app.get('port'), function(){
+  console.log("Express server listening on port " + app.get('port'));
+});
